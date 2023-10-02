88099744badaa549f206ed91a1277c1f1cdc584d
AkaTheDragon <146776153+AkaTheDragon@users.noreply.github.com>
2023-10-02 23:52:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var xmlDoc = xhr.responseXML;
-            var dragons = xmlDoc.getElementsByTagName("dragon");
+            var dragons = xmlDoc.getElementsByTagName("dragonName");
 
             // Handle search input
             var searchInput = document.getElementById("searchInput");
