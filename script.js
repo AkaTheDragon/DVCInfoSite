75be504de39a0e52716864e923fd28b4aa30efe9
AkaTheDragon <146776153+AkaTheDragon@users.noreply.github.com>
2023-10-07 13:01:32 +0200

    t

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function parseNestedTable(nestedTable) {
 }
 document.getElementById("popup").showpopup = function() {
   document.getElementById("popup").style.display = "block";
-  document.getElementById('iframe').src = "http://example.com";
+  document.getElementById('iframe').src = "index.html";
   document.getElementById('page').className = "darken";
   document.getElementById("page").style.display = "block";
 }
