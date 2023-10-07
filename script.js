36d09adba9a156bdb029861813aced160def7bb8
AkaTheDragon <146776153+AkaTheDragon@users.noreply.github.com>
2023-10-07 23:16:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -121,40 +121,40 @@ function parseNestedTable(nestedTable) {
 }
 
 
-//document.getElementById("a").onclick = function(e) {
-//  e.preventDefault();
-//  document.getElementById("popup").showpopup();
-//}
-
-//document.getElementById("popup").showpopup = function() {
-//  document.getElementById("popup").style.display = "block";
-//  document.getElementById("iframe").src = "dragoninfo.html";
-//  document.getElementById("page").className = "darken";
-//  document.getElementById("page").style.display = "block";
-//}
-
-//document.getElementById("page").onclick = function() {
-// if(document.getElementById("popup").style.display == "block") {
-//    document.getElementById("popup").style.display = "none";
-//    document.getElementById("page").style.display = "none";
-//    document.getElementById("page").className = "";
-//  }
-//};
-
-
 document.getElementById("a").onclick = function(e) {
-  e.preventDefault();
-  var isInit = true; // indicates if the popup already been initialized.
-  var isClosed = false; // indicates the state of the popup
-  document.getElementById("popup").style.display = "block";
-  document.getElementById('iframe').src = "dragoninfo.html";
-  document.getElementById('page').className = "darken";
-  document.getElementById('page').onclick = function() {
-    if(isInit){isInit=false;return;}
-    if(isClosed){return;} //if the popup is closed, do nothing.
-    document.getElementById("popup").style.display = "none";
-    document.getElementById('page').className = "";
-    isClosed=true;
-  }
-  return false;
+ e.preventDefault();
+ document.getElementById("popup").showpopup();
 }
+
+document.getElementById("popup").showpopup = function() {
+ document.getElementById("popup").style.display = "block";
+ document.getElementById("iframe").src = "dragoninfo.html";
+ document.getElementById("page").className = "darken";
+ document.getElementById("page").style.display = "block";
+}
+
+document.getElementById("page").onclick = function() {
+if(document.getElementById("popup").style.display == "block") {
+   document.getElementById("popup").style.display = "none";
+   document.getElementById("page").style.display = "none";
+   document.getElementById("page").className = "";
+ }
+};
+
+
+// document.getElementById("a").onclick = function(e) {
+//   e.preventDefault();
+//   var isInit = true; // indicates if the popup already been initialized.
+//   var isClosed = false; // indicates the state of the popup
+//   document.getElementById("popup").style.display = "block";
+//   document.getElementById('iframe').src = "dragoninfo.html";
+//   document.getElementById('page').className = "darken";
+//   document.getElementById('page').onclick = function() {
+//     if(isInit){isInit=false;return;}
+//     if(isClosed){return;} //if the popup is closed, do nothing.
+//     document.getElementById("popup").style.display = "none";
+//     document.getElementById('page').className = "";
+//     isClosed=true;
+//   }
+//   return false;
+// }
