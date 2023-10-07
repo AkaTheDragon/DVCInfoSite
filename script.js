b4274845f88d00f055d321ea813497f1a3848d14
AkaTheDragon <146776153+AkaTheDragon@users.noreply.github.com>
2023-10-07 13:10:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-document.getElementById("popup").showpopup = function() {
-  document.getElementById("popup").style.display = "block";
-  document.getElementById('iframe').src = "http://www.google.com";
-  document.getElementById('page').className = "darken";
-  document.getElementById("page").style.display = "block";
-}
-
-document.getElementById("a").onclick = function(e) {
-  e.preventDefault();
-  document.getElementById("popup").showpopup();
-}
-
-document.getElementById('page').onclick = function() {
-  if(document.getElementById("popup").style.display == "block") {
-    document.getElementById("popup").style.display = "none";
-    document.getElementById("page").style.display = "none";
-    document.getElementById('page').className = "";
-  }
-};
 
 
 document.addEventListener("DOMContentLoaded", function () {
