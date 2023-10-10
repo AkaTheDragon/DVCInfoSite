3c7c2ca936337f69192bf29a4039ddeb1badcea9
AkaTheDragon <146776153+AkaTheDragon@users.noreply.github.com>
2023-10-10 17:47:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -120,47 +120,49 @@ function parseNestedTable(nestedTable) {
     return table;
 }
 
-window.onload = function(){ 
-document.getElementsByClassName("a").onclick = function(e) {
-  e.preventDefault();
-  document.getElementById("popup").showpopup();
-}
+window.onload=AddClickEvent;
+
+
+function AddClickEvent(){
+    document.getElementsByClassName("a").addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("popup").showpopup();
+    })
 };
 
-// window.onload = function(){ 
-// document.getElementsByClassName("a").onclick = function(e) {
-//   e.preventDefault();
-//   var isInit = true; // indicates if the popup already been initialized.
-//   var isClosed = false; // indicates the state of the popup
-//   document.getElementById("popup").style.display = "block";
-//   document.getElementById('iframe').src = "dragoninfo.html";
-//   document.getElementById('page').className = "darken";
-//   document.getElementById('page').onclick = function() {
-//     if(isInit){isInit=false;return;}
-//     if(isClosed){return;} //if the popup is closed, do nothing.
-//     document.getElementById("popup").style.display = "none";
-//     document.getElementById('page').className = "";
-//     isClosed=true;
-//   }
-//   return false;
-// }
+
+// function AddClickEvent(){
+    // document.getElementsByClassName("a").addEventListener("click", function(event) {
+      // event.preventDefault();
+      // var isInit = true; // indicates if the popup already been initialized.
+      // var isClosed = false; // indicates the state of the popup
+      // document.getElementById("popup").style.display = "block";
+      // document.getElementById('iframe').src = "dragoninfo.html";
+      // document.getElementById('page').className = "darken";
+      // document.getElementById('page').onclick = function() {
+        // if(isInit){isInit=false;return;}
+        // if(isClosed){return;} //if the popup is closed, do nothing.
+        // document.getElementById("popup").style.display = "none";
+        // document.getElementById('page').className = "";
+        // isClosed=true;
+      // }
+      // return false;
+    // }
 // };
 
-window.onload = function(){ 
+    
 document.getElementById("popup").showpopup = function() {
- document.getElementById("popup").style.display = "block";
- document.getElementById("iframe").src = "dragoninfo.html";
- document.getElementById("page").className = "darken";
- document.getElementById("page").style.display = "block";
-}
-};
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("iframe").src = "dragoninfo.html";
+    document.getElementById("page").className = "darken";
+    document.getElementById("page").style.display = "block";
+    }
 
-window.onload = function(){ 
-document.getElementById("page").onclick = function() {
-if(document.getElementById("popup").style.display == "block") {
-   document.getElementById("popup").style.display = "none";
-   document.getElementById("page").style.display = "none";
-   document.getElementById("page").className = "";
- }
-};
+
+document.getElementById("page").addEventListener("click", function() {      
+    if(document.getElementById("popup").style.display == "block") {
+        document.getElementById("popup").style.display = "none";
+        document.getElementById("page").style.display = "none";
+        document.getElementById("page").className = "";
+    }   
 };
